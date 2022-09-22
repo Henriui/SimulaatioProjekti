4b4f98c326ba3c3c4f1f1b07c5ad21abf316e114
Henriui <henri.uimonen@gmail.com>
2022-09-22 22:24:28 +0300

    Revive jonnes portion

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,12 +1,9 @@
 package com.project;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
