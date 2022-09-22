657d971a1590bc1bbe57c70164228677654570cb
Cl0wnFiesta <clownfiesta98@gmail.com>
2022-09-22 16:11:15 +0300

    Uuden simulaation aloittamis näkymä

--- GraafinenLiittyma/src/main/java/com/example/UusiSimulaatioController.java
+++ GraafinenLiittyma/src/main/java/com/example/UusiSimulaatioController.java
@@ -2,11 +2,28 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class UusiSimulaatioController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
+    private Label yksityisJonossa;
+    @FXML
+    private Label YritysJonossa;
+
+    @FXML
+    private void takaisinMainView() throws IOException {
         App.setRoot("mainView");
+    }
+
+    @FXML
+    public void aloitaSimulaatio(){
+        for(int i = 0; i<1000;i++){
+            ilmoitaJononKoko(i);
+        }
+    }
+    public void ilmoitaJononKoko(int koko){
+        String tulos = String.valueOf(koko);
+        yksityisJonossa.setText(tulos);
     }
 }
