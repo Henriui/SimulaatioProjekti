c5b87cf8ee1218089d9671922041292f4cffcd8b
Cl0wnFiesta <clownfiesta98@gmail.com>
2022-09-24 14:29:32 +0300

    all works

--- src/main/java/com/project/SecondaryController.java
+++ src/main/java/com/project/SecondaryController.java
@@ -1,6 +1,13 @@
 package com.project;
 
 import java.io.IOException;
+
+import com.project.simu.framework.Moottori;
+import com.project.simu.framework.Trace;
+import com.project.simu.framework.Trace.Level;
+import com.project.simu.model.OmaMoottori;
+import com.project.simu.model.UserParametrit;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -28,13 +35,15 @@ public class SecondaryController {
 
     @FXML
     public void aloitaSimulaatio(){
-        //Trace.setTraceLevel(Level.INFO);
-		//Moottori m = new OmaMoottori();
-		//m.setSimulointiaika(1000);
-		//m.start();
+        UserParametrit uP = UserParametrit.getInstance();
+		Trace.setTraceLevel(Level.INFO);
+		Moottori m = new OmaMoottori();
+		m.setSimulointiaika(uP.getSimulaationAika());
+		m.start();
     }
     public void ilmoitaJononKoko(int koko){
         String tulos = String.valueOf(koko);
         yksityisJonossa.setText(tulos);
     }
+    
 }
