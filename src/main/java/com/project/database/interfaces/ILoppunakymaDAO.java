eac5523709f868f238be2ea67f0cd93b526c897b
Henriui <henri.uimonen@gmail.com>
2022-09-29 11:15:36 +0300

    save

--- src/main/java/com/project/database/interfaces/ILoppunakymaDAO.java
+++ src/main/java/com/project/database/interfaces/ILoppunakymaDAO.java
@@ -1,6 +1,10 @@
 package com.project.database.interfaces;
 
 public interface ILoppunakymaDAO {
+    // Connection open/close
     public abstract boolean openConnection();
     public abstract boolean closeConnection();
+
+    //
+    public abstract boolean addSuure();
 }
