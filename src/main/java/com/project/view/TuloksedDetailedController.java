4295f7e96862dec1a2ca9c71fc8b6953056cdb12
lassib <91030337+lassib@users.noreply.github.com>
2022-10-06 19:24:07 +0300

    FXML liitoksia

--- src/main/java/com/project/view/TuloksedDetailedController.java
+++ src/main/java/com/project/view/TuloksedDetailedController.java
@@ -5,40 +5,42 @@ import com.project.simu.model.PalveluPisteTulokset;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class TuloksedDetailedController {
-
     private NewSimulationController controller = new NewSimulationController();
-
     @FXML
     private Label kestoLabel;
-
     @FXML
     private Label pProsenttiLabel;
-
     @FXML
     private Label asMaaraLabel;
-
     @FXML
     private Label palvellutAsLabel;
-
     @FXML
     private Label poistuneetAsLabel;
-
     @FXML
     private Label uudelleenOhjAsLabel;
-
     @FXML
     private Label keskiLapiMenoAikLabel;
-
     @FXML
     private TableView<PalveluPisteTulokset> yritysPisteetTable;
-
+    @FXML
+    private TableColumn<PalveluPisteTulokset, String> yriPisteColumn;
+    @FXML
+    private TableColumn<PalveluPisteTulokset, String> yriKplColumn;
+    @FXML
+    private TableColumn<PalveluPisteTulokset, String> yriPalveluProColumn;
     @FXML
     private TableView<PalveluPisteTulokset> yksityisPisteetTable;
-
+    @FXML
+    private TableColumn<PalveluPisteTulokset, String> yksPisteColumn;
+    @FXML
+    private TableColumn<PalveluPisteTulokset, String> yksKplColumn;
+    @FXML
+    private TableColumn<PalveluPisteTulokset, String> yksPalveluProColumn;
     @FXML
     private Button removeButton;
     @FXML
