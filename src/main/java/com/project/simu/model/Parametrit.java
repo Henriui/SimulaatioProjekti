b1c14ed6726feb22b9a48b73ccb213693efc83aa
lassib <91030337+lassib@users.noreply.github.com>
2022-10-11 21:30:45 +0300

    More unnesesary code

--- src/main/java/com/project/simu/model/Parametrit.java
+++ src/main/java/com/project/simu/model/Parametrit.java
@@ -41,11 +41,6 @@ public class Parametrit {
     // Puhelinvalikkojen keskimääräinen palveluaika
     private double pValikkoAika;
 
-    // Tietokannan käyttäjäparametrit
-    private String dbName;
-    private String username;
-    private String password;
-
     public Parametrit() {
         setDefaultArvot();
     }
@@ -228,35 +223,5 @@ public class Parametrit {
 
         System.out.println("asTyyppiArr: " + Arrays.toString(asTyyppiArr));
         this.asTyyppiArr = asTyyppiArr;
-    }
-    // dbName, tableName ,username, password get/set
-
-    public String getDbName() {
-        return dbName;
-    }
-
-    public void setDbName(String dbName) {
-        this.dbName = dbName;
-    }
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setDbParameters(String dbName, String username, String password) {
-        this.dbName = dbName;
-        this.username = username;
-        this.password = password;
     }
 }
